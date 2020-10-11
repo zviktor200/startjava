ee0f844afa5cfd2b37b5155cd4faf5e76132eede
zviktor <viktor.zinoviev18@icloud.com>
2020-10-11 17:40:58 +0300

    HW_1

---
+++ Lesson1/Calculator.java
@@ -0,0 +1,23 @@
+public class Calculator {
+	public static void main(String[] args) {
+		int number1 = 4;
+		int number2 = 3;
+		char operation = '^';
+		if (operation == '*') {
+			System.out.println(number1 * number2);
+		} else if (operation == '+') {
+			System.out.println(number1 + number2);
+		} else if (operation == '-') {
+			System.out.println(number1 - number2);
+		} else if (operation == '^') {
+			int result = 1;
+			for (int i = 1; i <= number2; i++) {
+				result = result * number1;
+			}
+			System.out.println (result);
+		} else if (operation == '/') {
+			System.out.println(number1 / number2);
+		} else
+			System.out.println("ERROR");	
+	}
+}
